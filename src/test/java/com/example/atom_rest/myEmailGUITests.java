a0adfd47dbf5d0a234a1710724e1664790b3cec5
Atom-mrbird <ataberkyakar@outlook.com>
2022-05-07 09:38:03 +0300

    Add files via upload

---
+++ src/test/java/com/example/atom_rest/myEmailGUITests.java
@@ -0,0 +1,13 @@
+package com.example.atom_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class myEmailGUITests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
